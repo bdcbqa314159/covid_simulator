c5de90c667c24d5212af7e40acef9a46c36ec835
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2024-09-18 00:11:54 +0100

    progress no.2: adding some code

---
+++ app/location.cpp
@@ -0,0 +1,46 @@
+#include "location.hpp"
+#include "simulator.hpp"
+#include <cmath>
+
+#define CLOSE_ENOUGH 0.05
+
+Location::Location()
+    : x_(arc4random_uniform(X_LIMIT)), y_(arc4random_uniform(Y_LIMIT)) {}
+
+Location::Location(double nx, double ny) : x_(nx), y_(ny) {}
+
+bool Location::move_toward(Location destination, double howFar) {
+  double dx = destination.x_ - x;
+  double dy = destination.y_ - y;
+  double theta = atan2(dy, dx);
+
+  double distance = sqrt(dx * dx + dy * dy);
+
+  if (distance < howFar) {
+    x_ = destination.x_;
+    y_ = destination.y_;
+    return true;
+  } else {
+    x_ += (howFar * cos(theta));
+    y_ += (howFar * sin(theta));
+    return false;
+  }
+}
+
+double Location::get_distance(Location destination) {
+  double dx = destination.x_ - x;
+  double dy = destination.y_ - y;
+
+  double distance = sqrt(dx * dx + dy * dy);
+
+  return distance;
+}
+
+bool Location::at_location(Location destination) {
+  return (get_distance(destination) < CLOSE_ENOUGH);
+}
+
+double Location::get_x() { return x_; }
+double Location::get_y() { return y_; }
+void Location::set_x(double nx) { x_ = nx; }
+void Location::set_y(double ny) { y_ = ny; }
---
+++ app/location.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+class Location {
+private:
+  double x_{}, y_{};
+
+public:
+  Location();
+  Location(double nx, double ny);
+
+  double get_x();
+  double get_y();
+  void set_x(double nx);
+  void set_y(double ny);
+
+  double get_distance(Location other_location);
+  bool move_toward(Location destination, double speed);
+  bool at_location(Location destination);
+};
