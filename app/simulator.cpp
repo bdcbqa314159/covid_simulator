0ff2d062562d4e8a339adb01df6f08b0367823eb
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2024-09-18 20:16:16 +0100

    progress no.11: cleaning a bit

--- app/simulator.cpp
+++ app/simulator.cpp
@@ -16,11 +16,10 @@ u_int32_t arc4random_uniform(u_int32_t upper_bound);
 
 bool try_event(double probability) {
   assert(probability >= 0.0 && probability <= 1.0);
-  const int resolution =
-      100000; // probably doesn't matter what this is, as long as it's big
+  const int resolution = 100000;
 
   double rnum = arc4random_uniform(resolution);
-  rnum = rnum / (double)resolution;
+  rnum = rnum / static_cast<double>(resolution);
   return rnum <= probability;
 }
 
